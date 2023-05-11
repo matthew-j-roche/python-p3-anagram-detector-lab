6328b403b2bf616a3c595967a08364d099455b7e
matt roche <matt.roche@gmail.com>
2023-05-11 05:39:53 -0500

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
-# your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        return [w for w in word_list if sorted([letter for letter in w.lower()]) == sorted([letter for letter in self.word.lower()])]
+
+    
